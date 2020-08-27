c4a2783ce09b72642ef7fd1b14319b5e5b4135db
Jorge Garcia <jorge.gar.per@gmail.com>
2020-08-27 11:37:27 +0200

    done module before http requests

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:shop_app/screens/edit_product_screen.dart';
+
+import '../providers/products.dart';
 
 class UserProductItem extends StatelessWidget {
   final String title;
@@ -28,7 +32,9 @@ class UserProductItem extends StatelessWidget {
             ),
             IconButton(
               icon: Icon(Icons.delete),
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<Products>(context, listen: false).deleteProduct(id);
+              },
               color: Theme.of(context).errorColor,
             ),
           ],
